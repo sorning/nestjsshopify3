cf929b27b0c124e374b9ee96ace9c693e8aad2eb
willingWoods <willingwind@icloud.com>
2023-06-17 16:37:51 +0800

    6

---
+++ app/firebase6/page.js
@@ -0,0 +1,92 @@
+'use client'
+import { useState,useEffect } from "react"
+import {collection, query, addDoc, onSnapshot, deleteDoc, doc} from 'firebase/firestore'
+import {db} from '../../lib/firebase'
+
+export default function firebasefirestore6() {
+    const [items, setItems]=useState([
+        {name:'coffe',price:'4'},
+        {name:'tea', price:'9'}
+    ])
+    const [total, setTotal]=useState(0)
+    //add newitem to database
+    const [newItem, setNewItem]=useState({name:'',price:''})
+    const addItem= async (e)=>{
+        e.preventDefault();
+        if (newItem.name!='' && newItem.price!='') {
+            await addDoc(collection(db,'items'),{
+                name:newItem.name.trim(),
+                price:newItem.price
+            })
+            setNewItem({name:'',price:''})
+        }
+    }
+    //read items from database using query unsubscribe onsnapshot, we use setItems(itemsArry) to accumulate the items.
+    useEffect(()=>{
+        const q=query(collection(db,'items'))
+        const unsubscribe=onSnapshot(q,(querysnapshot)=>{
+            const itemsArray=[]
+            querysnapshot.forEach((doc)=>{
+                itemsArray.push({...doc.data(),id:doc.id})
+            });
+            setItems(itemsArray);
+            //calculate totalprice
+            // const calculateTotal=()=>{
+            //     const totalprice=itemsArray.reduce((sum,item)=>sum+parseFloat(item.price),0);
+            //     setTotal(totalprice);
+            // }
+            // calculateTotal();
+            const totalprice=itemsArray.reduce((sum,item)=>sum+parseFloat(item.price),0)
+            setTotal(totalprice);
+            return ()=>unsubscribe();
+        })
+    },[])
+    //delete item from database
+        const deleteItem=async (id)=>{
+            await deleteDoc(doc(db,'items',id))
+        }
+    return (
+        <>
+            <main className='flex flex-col justify-between min-h-screen items-center sm:p-24 p-4'>
+                <div className='items-center justify-between z-10 w-full font-mono text-sm'>
+                    <h1 className='text-center w-full text-4xl p-4 '>Expense Tracker</h1>
+                    <div className='bg-slate-800 p-4 rounded-lg'>
+                        <form className='grid grid-cols-6 items-center text-black'>
+                            <input 
+                            value={newItem.name}
+                            onChange={(e)=>setNewItem({...newItem,name:e.target.value})}
+                            className='col-span-3 p-3 border' type='text' placeholder='Enter Item'></input>
+                            <input 
+                            value={newItem.price}
+                            onChange={(e)=>setNewItem({...newItem,price:e.target.value})}
+                            className='col-span-2 p-3 border mx-3' type='text' placeholder='Enter $'></input>
+                            <button 
+                            onClick={addItem}
+                            type='submit'
+                            className='bg-slate-950  text-white p-3 text-xl hover:bg-slate-900' >+</button>
+                        </form>
+                        <ul>
+                            {items.map((item)=>(
+                                <li className='bg-slate-950 my-4 w-full flex justify-between '>
+                                    <div className='flex justify-between w-full p-4'>
+                                        <span className='capitalize'>{item.name.trim()}</span>
+                                        <span>${item.price}</span>
+                                    </div>
+                                    <button
+                                    onClick={()=>(deleteItem(item.id))}
+                                    className='ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16'>X</button>
+                                </li>
+                            ))}
+                        </ul>
+                        {(items.length<0)?(''):(
+                            <div className='flex justify-between p-3'>
+                                <span>Total</span>
+                                <span>${total}</span>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </main>
+        </>
+    )
+}
---
+++ app/shopifyproductquery/page.js
@@ -0,0 +1,51 @@
+// import { fragment } from 'react';
+import Navbar from "../../components/layout/navbar/navbar";
+import Footer from "../../components/layout/footer/footer";
+// import About from "@/pages/graph";
+import { ShopifyFetch } from "@/lib/shopifyquery";
+import Image from "next/image";
+
+export default async function Home() {
+    const query1 = `
+    query Products {
+        products(first: 6) {
+          edges {
+            node {
+              handle
+              images(first: 6) {
+                edges {
+                  node {
+                    altText
+                    transformedSrc
+                  }
+                }
+              }
+              title
+              tags
+            }
+          }
+        }
+      }`
+      ;
+    const data = await ShopifyFetch(query1);
+
+    const title1 = data.data.products.edges[0];
+
+    return (
+        <>
+            <Navbar />
+                <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+                    <p>hello world. {title1.node.title}</p>
+                    <p>hello world. {title1.node.images.edges[0].node.transformedSrc}</p>
+                    <Image 
+                        src = {title1.node.images.edges[0].node.transformedSrc}
+                        // src = 'https://cdn.shopify.com/s/files/1/0421/0242/3709/products/AmazoniteGravel1.jpg?v=1603792822'
+                        width={500}
+                        height={500}
+                        alt='hi'
+                    />
+                </div>
+            <Footer />
+        </>
+    )
+}
